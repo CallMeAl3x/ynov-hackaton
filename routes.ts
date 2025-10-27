81db2760aa6c8fc614ffa271543a2bb1e101ee89
alexa <alexandre.py.etu@univ-lille.fr>
2025-10-27 16:58:11 +0100

    feat(ia): get ollama response

--- routes.ts
+++ routes.ts
@@ -5,3 +5,5 @@ export const authRoutes = ["/auth/register", "/auth/login", "/auth/error"];
 export const apiAuthPrefix = "/api/auth";
 
 export const DEFAULT_LOGIN_REDIRECT = "/onboarding";
+
+export const TEXTAREA_ROUTE = "/textarea";
